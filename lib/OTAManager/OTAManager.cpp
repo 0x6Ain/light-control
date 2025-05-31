e5cc584b117ae20e2b42e6ed291c508ebf6a4abc
jinyoung <jinpods@gmail.com>
2025-05-31 18:55:40 +0900

    feat: delete ota manager print

--- lib/OTAManager/OTAManager.cpp
+++ lib/OTAManager/OTAManager.cpp
@@ -22,18 +22,8 @@ void OTAManager::handle()
 
 void OTAManager::setupCallbacks()
 {
-    ArduinoOTA.onStart([]()
-                       { Serial.println("OTA 시작"); });
-    ArduinoOTA.onEnd([]()
-                     { Serial.println("\nOTA 완료"); });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-                          { Serial.printf("OTA 진행: %u%%\r", (progress / (total / 100))); });
-    ArduinoOTA.onError([](ota_error_t error)
-                       {
-        Serial.printf("OTA 에러[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("인증 실패");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("초기화 실패");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("연결 실패");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("데이터 수신 실패");
-        else if (error == OTA_END_ERROR) Serial.println("종료 실패"); });
+    ArduinoOTA.onStart([]() {});
+    ArduinoOTA.onEnd([]() {});
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {});
+    ArduinoOTA.onError([](ota_error_t error) {});
 }
